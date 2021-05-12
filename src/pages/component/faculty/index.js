c51abcb6b718770a9ddbce457f6cc5af2765ceb3
pengshunlong <pengshunlong@wakedata.com>
2021-05-12 10:59:47 +0800

    提交代码

--- src/pages/component/faculty/index.js
+++ src/pages/component/faculty/index.js
@@ -13,38 +13,26 @@ const Faculty = () => {
       ></HeadLine>
       <div className="info">
         <div>
-          <div>
-            <img src={imgFaculty1} alt="" />
-          </div>
-          <h2>张晶</h2>
+          <img src={imgFaculty1} alt="" />
           <h3>北师大教育心理学硕士</h3>
           <p>
             多次在《人民日报》等核心刊物发表学术文章，STEAM教育及图形化编程教育资深专家
           </p>
         </div>
         <div>
-          <div>
-            <img src={imgFaculty2} alt="" />
-          </div>
-          <h2>夏立</h2>
+          <img src={imgFaculty2} alt="" />
           <h3>香港理工大学博士</h3>
           <p>
             中国教育技术协会人工智能专委会理事，《青少年编程能力等级标准》召集人及标准主要起草人
           </p>
         </div>
         <div>
-          <div>
-            <img src={imgFaculty3} alt="" />
-          </div>
-          <h2>苏扬扬</h2>
+          <img src={imgFaculty3} alt="" />
           <h3>香港大学教育学博士</h3>
           <p>10年教育工作经验，累计服务超过50个国家的学生，千人直播课名师</p>
         </div>
         <div>
-          <div>
-            <img src={imgFaculty4} alt="" />
-          </div>
-          <h2>雷晨</h2>
+          <img src={imgFaculty4} alt="" />
           <h3>国家心理学咨询师资格</h3>
           <p>
             7年儿童性格教育经验，“十三五”规划教材《儿童性格涵养教学法简明教程》编写者之一”
